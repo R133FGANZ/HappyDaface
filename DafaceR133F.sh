32f08b5495143125ad455603eb474f7313c6474a
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-09-07 11:39:26 +0700

    Update

--- DafaceR133F.sh
+++ DafaceR133F.sh
@@ -2,10 +2,12 @@ clear
 
 toilet -f slant --gay "Dafacer"
 sleep 3
-echo "\033[32;1mauthor: Mr.4R13F"
-echo "\033[33;1mTeam: Devlin Cyber Team"
-echo "\033[34;1mIndonesia Dafacer Team"
-echo "\033[32;1mJawaTimur Black hat"
+echo "\033[1;33m[=========================]"
+echo "\033[32;1m author: Mr.4R13F"
+echo "\033[33;1m Team: Devlin Cyber Team"
+echo "\033[34;1m Indonesia Dafacer Team"
+echo "\033[32;1m JawaTimur Black hat"
+echo "\033[1;33m[==========================]"
 echo "Masukan nama script"
 read -p "[=======>" script
 echo "\033[32;1m"
@@ -20,4 +22,4 @@ sleep 4
 echo "Silahkan Cek Web $Ke1/$script"
 sleep 1
 
-echo "[=============HAPPY DEFACE=============]"
+echo "[=======================HAPPY DEFACE======================]"
